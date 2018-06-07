d7671ec31289852dbcbe9957d39688e3410c6d9d
ingorewho <17761256463@163.com>
2018-06-07 17:21:38 +0800

    springboot集成redis

--- springboot-redis/src/main/java/redis/service/RedisServiceImpl.java
+++ springboot-redis/src/main/java/redis/service/RedisServiceImpl.java
@@ -4,11 +4,18 @@
 package redis.service;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -26,26 +33,23 @@ public class RedisServiceImpl
 	private RedisTemplate redisTemplate;
 	
 	/**
-	 * 获取数据
+	 * 存放实体对象：实体对象一定要实现序列化接口
 	 * @param key .
 	 * @param value .
 	 * @throws Exception .
 	 */
-	public void put(String key, Object value) throws Exception
+	public void putEntity(String key, Object value) throws Exception
 	{
-		try
+		if(StringUtils.isEmpty(key))
 		{
-			ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
-			operations.set(key, value);
+			return;
 		}
-		catch(Exception e)
-		{
-			logger.error(String.format("存放数据到redis异常: key=%s, value=%s", key, value), e);
-		}
+		ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
+		operations.set(key, value);
 	}
 	
 	/**
-	 * 存放数据，可设置有效期
+	 * 存放实体对象，可设置有效期
 	 * @param key .
 	 * @param value .
 	 * @param expireTime .
@@ -53,25 +57,96 @@ public class RedisServiceImpl
 	 */
 	public void put(String key, Object value, long expireTime) throws Exception
 	{
-		try
+		if(StringUtils.isEmpty(key))
 		{
-			ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
-			operations.set(key, value);
-			redisTemplate.expire(key, 1, TimeUnit.MINUTES);
+			return;
 		}
-		catch(Exception e)
-		{
-			logger.error(String.format("存放数据到redis异常: key=%s, value=%s, expireTime=%s", key, value, expireTime), e);
-		}
+		ValueOperations<Serializable, Object> operations = redisTemplate.opsForValue();
+		operations.set(key, value);
+		redisTemplate.expire(key, 1, TimeUnit.MINUTES);
 	}
 	
 	/**
-	 * 存放数据
+	 * 存放集合数据
+	 * @param key .
+	 * @param values .
+	 * @throws Exception .
+	 */
+	public void putSet(String key, Set<String> values)throws Exception
+	{
+		if(StringUtils.isEmpty(key) || values == null || values.isEmpty())
+		{
+			return;
+		}
+		
+		SetOperations setOps = redisTemplate.opsForSet();
+		
+		setOps.add(key, values);
+	}
+	
+	/**
+	 * 存放列表数据
+	 * @param key .
+	 * @param values .
+	 * @throws Exception .
+	 */
+	public void putList(String key, List<String> values)throws Exception
+	{
+		if(StringUtils.isEmpty(key) || values == null || values.isEmpty())
+		{
+			return;
+		}
+		
+		ListOperations listOps = redisTemplate.opsForList();
+		
+		listOps.leftPush(key, values);
+		
+		//or listOps.rightPush(key, values);
+	}
+	
+	/**
+	 * 存放map数据
+	 * @param key .
+	 * @param values .
+	 * @throws Exception .
+	 */
+	public void putHash(String key, Map<String, String> values)throws Exception
+	{
+		if(StringUtils.isEmpty(key) || values == null || values.isEmpty())
+		{
+			return;
+		}
+		
+		HashOperations hashOps = redisTemplate.opsForHash();
+		
+		hashOps.putAll(key, values);
+	}
+	
+	/**
+	 * 删除数据
 	 * @param key .
 	 * @return .
 	 * @throws Exception .
 	 */
-	public Object get(String key) throws Exception
+	public boolean delete(String key)throws Exception
+	{
+		if(StringUtils.isEmpty(key))
+		{
+			return false;
+		}
+		
+		redisTemplate.delete(key);
+		
+		return true;
+	}
+	
+	/**
+	 * 获取实体对象
+	 * @param key .
+	 * @return .
+	 * @throws Exception .
+	 */
+	public Object getEntity(String key) throws Exception
 	{
 		if(StringUtils.isEmpty(key))
 		{
@@ -82,4 +157,59 @@ public class RedisServiceImpl
 		Object result = operations.get(key);
 		return result;
 	}
+	
+	/**
+	 * 获取集合数据
+	 * @param key .
+	 * @return .
+	 * @throws Exception .
+	 */
+	public Set<String> getSets(String key)throws Exception
+	{
+		if(StringUtils.isEmpty(key))
+		{
+			return null;
+		}
+		
+		SetOperations setOps = redisTemplate.opsForSet();
+		
+		return setOps.members(key);
+	}
+	
+	/**
+	 * 获取列表数据
+	 * @param key .
+	 * @return .
+	 * @throws Exception .
+	 */
+	public List<String> getLists(String key)throws Exception
+	{
+		if(StringUtils.isEmpty(key))
+		{
+			return null;
+		}
+		
+		ListOperations listOps = redisTemplate.opsForList();
+		
+		return (List<String>) listOps.leftPop(key);
+	}
+	
+	/**
+	 * 获取map数据
+	 * @param key .
+	 * @return .
+	 * @throws Exception .
+	 */
+	public HashMap<String, String> getHashs(String key)throws Exception
+	{
+		if(StringUtils.isEmpty(key))
+		{
+			return null;
+		}
+		
+		HashOperations hashOps = redisTemplate.opsForHash();
+		
+		return (HashMap<String, String>) hashOps.entries(key);
+	}
+	
 }
--- springboot-redis/src/main/java/redis/test/TestRedisService.java
+++ springboot-redis/src/main/java/redis/test/TestRedisService.java
@@ -3,6 +3,13 @@
  */
 package redis.test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,15 +35,48 @@ public class TestRedisService
 	@Test
 	public void testPutGet()
 	{
-		String key = "test-key";
-		String value = "test-value";
+		
 		try
 		{
-			logger.info(String.format("向redis中添加数据: key=%s, value=%s", key, value));
-			redisService.put(key, value);
+			//1.测试实体对象存放和获取
+			String key = "test-key";
+			TestEntity entity = new TestEntity();
+			entity.setId(1);
+			entity.setName("test-name");
+			logger.info(String.format("向redis中添加实体对象数据: key=%s, value=%s", key, entity));
+			redisService.putEntity(key, entity);
+			TestEntity result = (TestEntity)redisService.getEntity(key);
+			logger.info(String.format("从redis中获取实体对象数据: key=%s, value=%s", key, result));
 			
-			Object result = redisService.get(key);
-			logger.info(String.format("从redis中获取数据: key=%s, value=%s", key, result));
+			//2.测试集合数据存放和获取
+			String key1 = "test-key1";
+			Set<String> sets = new HashSet<String>();
+			sets.add("test-value1");
+			sets.add("test-value2");
+			logger.info(String.format("向redis中添加集合数据: key=%s, value=%s", key1, sets));
+			redisService.putSet(key1, sets);
+			Set<String> result1 = redisService.getSets(key1);
+			logger.info(String.format("从redis中获取集合数据: key=%s, value=%s", key1, result1));
+			
+			//3.测试列表数据存放和获取
+			String key2 = "test-key2";
+			List<String> lists = new ArrayList<String>();
+			lists.add("test-value1");
+			lists.add("test-value2");
+			logger.info(String.format("向redis中添加列表数据: key=%s, value=%s", key2, lists));
+			redisService.putList(key2, lists);
+			List<String> result2 = redisService.getLists(key2);
+			logger.info(String.format("从redis中获取列表数据: key=%s, value=%s", key2, result2));
+			
+			//4.测试map数据的存放和获取
+			String key3 = "test-key3";
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("test1", "value1");
+			map.put("test2", "value2");
+			logger.info(String.format("向redis中添加map数据: key=%s, value=%s", key3, lists));
+			redisService.putHash(key3, map);
+			HashMap<String, String> result3 = redisService.getHashs(key3);
+			logger.info(String.format("从redis中获取map数据: key=%s, value=%s", key3, result3));
 		} 
 		catch (Exception e)
 		{
