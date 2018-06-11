618e718db743754d476dd1d769d3a2fb5eece42c
ingorewho <17761256463@163.com>
2018-06-11 08:52:48 +0800

    springboot动态修改日志级别

---
+++ springboot-logger/src/main/java/logger/LoggerApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ignore1992
+ *
+ */
+@SpringBootApplication
+public class LoggerApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(LoggerApplication.class, args);
+	}
+}
