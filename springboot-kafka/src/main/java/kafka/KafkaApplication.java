c47b1e3e90aac5f287d8d800a60f51eb7107e874
ingorewho <17761256463@163.com>
2018-06-19 10:56:46 +0800

    springboot-使用kafka发送接收消息

---
+++ springboot-kafka/src/main/java/kafka/KafkaApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ignore1992
+ *
+ */
+@SpringBootApplication
+public class KafkaApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(KafkaApplication.class, args);
+	}
+}
